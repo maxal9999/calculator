a36c80af8d1169cb3ef20a4c88bff49351b23b32
as.maksimovskiy <as.maksimovskiy@tensor.ru>
2020-08-18 22:59:43 +0300

    Add unit tests

---
+++ test/calculator_tests.cpp
@@ -0,0 +1,21 @@
+/**
+ * \file
+ * \brief Unit tests of the project
+ * \author Maksimovskiy A.S.
+ */
+
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE TestCalculator
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(TestCalculator) // Начало набора тестов
+
+
+BOOST_AUTO_TEST_CASE(TestHashDecoderFirst)
+{
+   BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // Конец набора тестов
